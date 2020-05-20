93ab8c0d8159b58917de9e9545cdf50871a3b140
yehudit <cyehudit10@gmail.com>
2020-05-20 09:09:38 +0300

    upload files

---
+++ WebApi_Brix/WebApi_Brix/BL/locations.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi_Brix.Models;
+
+namespace WebApi_Brix.BL
+{
+    public class Locations
+    {
+        public static List<Location> locations { get; set; }
+        //public Locations()
+        //{
+        //    locations = new List<Location>
+        //    {
+        //        new Location("Jerusalem",new DateTime(2005, 12, 12),new DateTime(2005, 11, 12),"school","111"),
+        //        new Location("Bney Brak",new DateTime(2004, 12, 12),new DateTime(2004, 11, 12),"park","111"),
+        //        new Location("Bney Brak",new DateTime(2004, 12, 12),new DateTime(2004, 11, 12),"library","111"),
+        //        new Location("Jerusalem",new DateTime(2005, 12, 12),new DateTime(2005, 11, 12),"restaurant","222"),
+        //    };
+        //}
+        public static async void LoadLocationsAsync()
+        {
+            try
+            {
+                locations = new List<Location>
+            {
+                new Location("Jerusalem",new DateTime(2005, 12, 12),new DateTime(2005, 11, 12),"school","111"),
+                new Location("Bney Brak",new DateTime(2004, 12, 12),new DateTime(2004, 11, 12),"park","111"),
+                new Location("Bney Brak",new DateTime(2004, 12, 12),new DateTime(2004, 11, 12),"library","111"),
+                new Location("Jerusalem",new DateTime(2005, 12, 12),new DateTime(2005, 11, 12),"restaurant","222"),
+            };
+            }
+            catch (Exception)
+            {
+                throw new Exception("load failed");
+            }
+
+        }
+        public static async Task<List<Location>> GetLocationsAsync()
+        {
+            try
+            {
+                return new List<Location>
+            {
+                new Location("Jerusalem",new DateTime(2005, 12, 12),new DateTime(2005, 11, 12),"school","111"),
+                new Location("Bney Brak",new DateTime(2004, 12, 12),new DateTime(2004, 11, 12),"park","111"),
+                new Location("Bney Brak",new DateTime(2004, 12, 12),new DateTime(2004, 11, 12),"library","111"),
+                new Location("Jerusalem",new DateTime(2005, 12, 12),new DateTime(2005, 11, 12),"restaurant","222"),
+            };
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("we didn't find");
+            }
+
+            return null;
+        }
+
+        public static async Task<List<Location>> GetLocationsByPatientAsync(String id)
+        {
+            try
+            {
+                if (locations.FirstOrDefault(l => l.patientId == id) != null)
+                {
+                    return locations.Where(i => i.patientId == id).ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("we didn't find");
+            }
+
+            return null;
+        }
+        public static async Task<List<Location>> AddLocationsAsync(List<Location> newLocations,string id)
+        {
+            try
+            {
+                //locations.RemoveAll(l => l.patientId == id);
+                locations.AddRange(newLocations);
+                return newLocations;
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("adding failed");
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ WebApi_Brix/WebApi_Brix/Controllers/LocationsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_Brix.Models;
+using WebApi_Brix.BL;
+namespace WebApi_Brix.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Location>>> Get()
+        {
+            try
+            {
+                Locations.LoadLocationsAsync();
+                //to do async ??
+                var results = await Locations.GetLocationsAsync();
+                return results;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "data failed");
+                //return BadRequest("get location failed");
+            }
+
+        }
+
+        [HttpGet("{patienId}")]
+        public async Task<ActionResult<List<Location>>> Get(string patienId)
+        {
+            try
+            {
+                var results = await Locations.GetLocationsByPatientAsync(patienId);
+                return results;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "data failed");
+                //return BadRequest("get location failed");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Location>>> Post(List<Location> newLocation)
+        {
+            try
+            {
+                //if i wont send patientId
+                //if (id == "")
+                //    id = newLocation[0].patientId;
+                var results = await Locations.AddLocationsAsync(newLocation, "111");
+                return results;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+
+
+}
