8587d7190e674e3a7501de4a76a43ff8bfd0e6f7
yehudit <cyehudit10@gmail.com>
2020-05-20 11:20:50 +0300

    desing web api

---
+++ WebApi_Brix/WebApi_Brix/BLL/locations.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi_Brix.Models;
+using static WebApi_Brix.Models.Data;
+
+namespace WebApi_Brix.BLL
+{
+    public class Locations
+    {
+      
+        public static async Task<List<Location>> GetLocationsAsync()
+        {
+            try
+            {
+                return locations;
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("we didn't find");
+            }
+
+            return null;
+        }
+
+        public static async Task<List<Location>> GetLocationsByPatientAsync(String id)
+        {
+            try
+            {
+                if (locations.FirstOrDefault(l => l.patientId == id) != null)
+                {
+                    return locations.Where(i => i.patientId == id).ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("we didn't find");
+            }
+
+            return null;
+        }
+        public static async Task<List<Location>> AddLocationsAsync(string patientId, List<Location> newLocations)
+        {
+            try
+            {
+                locations.RemoveAll(l => l.patientId == patientId);
+                locations.AddRange(newLocations);
+                return newLocations;
+            }
+            catch (Exception)
+            {
+                throw new Exception("adding failed");
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ WebApi_Brix/WebApi_Brix/Models/Data.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Brix.Models
+{
+    public static class Data
+    {
+        public static List<Location> locations { get; set; }
+
+        static Data()
+        {
+            locations = new List<Location>
+            {
+                new Location("Jerusalem",new DateTime(2005, 12, 12),new DateTime(2005, 11, 12),"school","111"),
+                new Location("Bney Brak",new DateTime(2004, 12, 12),new DateTime(2004, 11, 12),"park","111"),
+                new Location("Bney Brak",new DateTime(2004, 12, 12),new DateTime(2004, 11, 12),"library","111"),
+                new Location("Jerusalem",new DateTime(2005, 12, 12),new DateTime(2005, 11, 12),"restaurant","222"),
+            };
+        }
+
+        public class Location
+        {
+            public string city { get; set; }
+            public DateTime startDate { get; set; }
+            public DateTime endDate { get; set; }
+            public string location { get; set; }
+            public string patientId { get; set; }
+
+            public Location(string city, DateTime startDate, DateTime endDate, string location, string patientId)
+            {
+                this.city = city;
+                this.startDate = startDate;
+                this.endDate = endDate;
+                this.location = location;
+                this.patientId = patientId;
+
+            }
+
+        }
+    }
+}
